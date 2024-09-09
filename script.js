1549caf0f89efde0ad9dedb29d269afc6f93ccf3
Hamilguijr <163039166+Hamilguijr@users.noreply.github.com>
2024-09-09 18:23:15 -0300

    Upload da Atualização

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const avanca = document.querySelectorAll('.prox.botao');
 avanca.forEach(button => {
     button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
-        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-prox');
 
         atual.classList.remove('ativo');
         document.getElementById(proximoPasso).classList.add('ativo');
